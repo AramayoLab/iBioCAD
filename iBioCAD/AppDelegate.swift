b25fab72e6897c09bb90dff5386619c288b32b31
Rudy Aramayo <vzw.rudy.aramayo@outlook.com>
2018-01-05 07:53:10 -0600

    Initial Commit

---
+++ iBioCAD/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  iBioCAD
+//
+//  Created by Rudy Aramayo on 1/5/18.
+//  Copyright © 2018 OrbitusBiomedical. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
